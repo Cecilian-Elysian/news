5774db07c7c822af40686db9287c5c3950266b08
liyiming <lym18986730830@qq.com>
2026-04-19 00:04:27 +0800

    chore: bump version to 2.0.1

--- news-crawler.user.js
+++ news-crawler.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         新闻爬取器
 // @namespace    https://github.com/Cecilian-Elysian/news
-// @version      2.0.0
+// @version      2.0.1
 // @description  一键抓取新闻、自动生成日报并导出
 // @author       Cecilian-Elysian
 // @match        *://*/*
